23bf2f330ddf66dd5e7e2bad7b0e1a9092fe85fa
Simone <144659113+Simonesho@users.noreply.github.com>
2023-11-17 23:29:29 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,51 @@
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+    }
+        }
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+
+        }def generate_random_data():
+    for item in data:
+
+if __name__ == "__main__":
+
+    main()
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+    return dataimport random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    for item in data:
+if __name__ == "__main__":
+
+    return data
+        print(f"Random Number: {item}")
+
+    main()
+    data = generate_random_data()
+def generate_random_data():
+    }
+        }
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+
+        for (int item : data) {
+public class RandomDataGenerator {
+}
+        }
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
